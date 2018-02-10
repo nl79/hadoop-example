e09499af755e02dd57f8a839a33ddbe703a7897b
Nazar Lesiv <nazarlesiv@Nazars-MBP-2.home>
2018-02-10 13:56:39 -0500

    initial commit

---
+++ projects/1.dominant-terms.java
@@ -0,0 +1,5 @@
+/*
+  Computes how many times the words “education”, “politics”, “sports”, and “agriculture”
+  appear in each file. Then, the program outputs the number of states for which each of
+  these words is dominant (i.e., appears more times than the other three words)
+ */
---
+++ projects/2.ranking-by-terms.java
@@ -0,0 +1,6 @@
+/*
+  Identify all states that have the same ranking of these four words. For example, NY, NJ, PA
+  may have the ranking 1. Politics; 2. Sports. 3. Agriculture; 4. Education (meaning “politics”
+  appears more times than “sports” in the Wikipedia file of the state, “sports” appears more
+  times than “agriculture”, etc.)
+ */
